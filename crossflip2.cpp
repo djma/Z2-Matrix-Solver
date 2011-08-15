5e8c4415822f396355b2709d176c9725c0ee73b5
David Ma <ma.davidj@gmail.com>
2011-08-15 00:30:01 -0700

    moved MyBoolVector to separate file

--- crossflip2.cpp
+++ crossflip2.cpp
@@ -3,87 +3,14 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-//#include "MyBoolVector.h"
+#include "MyBoolVector.h"
 
 #define INTSIZE 32
 #define MININT -2147483648
 
 typedef int mybool;
 
-
 using namespace std;
-
-
-
-class MyBoolVector
-{
-public:
-    MyBoolVector( const int& size )
-        : _vector((size-1)/INTSIZE+1, 0)
-        , _sizeInBits(size)
-    {};
-
-    ~MyBoolVector()
-    {
-    };
-
-    const int& size() {return _sizeInBits;};
-
-    void myXOR( const MyBoolVector& vb, const int& begin = 0 )
-    {
-        int n = _vector.size();
-        for ( int i = begin/INTSIZE; i < n; i++ )
-            _vector[i] ^= vb._vector[i];
-    };
-
-    bool operator[] (const int& i)
-    {
-        //unsigned int division is faster than int division
-        return (( _vector[(unsigned int)i/INTSIZE] << ((unsigned int)i%INTSIZE) ) < 0); //two's complement check of first bit
-    };
-
-    bool back()
-    {
-        return (*this)[_sizeInBits - 1];
-    };
-
-    void mySwap( MyBoolVector& vb)
-    {
-        _vector.swap(vb.getVector());
-    };
-
-    void flipBit( const int& i )
-    {
-        _vector[i/INTSIZE] ^= (MININT >> (i%INTSIZE));
-    };
-
-    void flipON( const int& i )
-    {
-        _vector[i/INTSIZE] |= (MININT >> (i%INTSIZE));
-    };
-
-    void flipOFF( const int& i )
-    {
-        _vector[i/INTSIZE] &= ~(MININT >> (i%INTSIZE));
-    };
-
-    std::vector<int>& getVector()
-    {
-        return _vector;
-    }
-
-private:
-    MyBoolVector();
-
-    int _sizeInBits;
-    std::vector<int> _vector;
-
-};
-
-
-
-
-
 
 
 void rowReduceInZ_2( std::vector< MyBoolVector > &matrix )
